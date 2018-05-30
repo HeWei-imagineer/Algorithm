73d0ea5b7c91ec40964caf0b98fbbf4fd25bc9a0
HeWei-imagineer <summer.vv@outlook.com>
2018-05-30 15:59:50 +0800

    tree level

---
+++ Leetcode/039_Combination_Sum.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+	let ans = [];
+	findCurcle(candidates, target, [], ans);
+	console.log(ans);
+    
+};
+
+let findCurcle = function(candidates, target, temp, ans) {
+	if (target === 0) {
+		return true;
+	}
+	if (candidates.length > 0) {
+		let addSum = [];
+		for (let i = 0; target - candidates[0]*i >= 0; i++) {
+			if (i > 0) {
+				addSum.push(candidates[0]);
+			}
+			if (findCurcle(candidates.slice(1,), target - candidates[0]*i, [...temp, ...addSum], ans)) {
+				ans.push( [...temp, ...addSum]);
+				console.log(ans[ans.length-1]);
+			};
+		}
+
+	}
+	return false;
+}
+// congraduation! once time ac, but I still use the old way~ 
+combinationSum([2,3,5],8);
